66813dd9ec855d4d47b26ae938d1007cacd97b7a
Alexander Konovalov <shuryak@protonmail.com>
2020-05-19 11:59:04 +0300

    Added creator type output to console

--- creational/FactoryMethod/Client.cs
+++ creational/FactoryMethod/Client.cs
@@ -8,18 +8,18 @@ namespace FactoryMethod
     {
         public void Main()
         {
-            Console.WriteLine("App: Start ConcreteCreator1");
+            Console.WriteLine("App: Start SomeCreator1");
             StartCreator(new SomeCreator1());
             
             Console.WriteLine("");
             
-            Console.WriteLine("App: Start ConcreteCreator2");
+            Console.WriteLine("App: Start SomeCreator2");
             StartCreator(new SomeCreator2());
         }
 
         private void StartCreator(Creator creator)
         {
-            Console.WriteLine("Client: Start creator.SomeOperation.\n" + creator.SomeOperation());
+            Console.WriteLine("Client: Start " + creator.GetType().Name + ".SomeOperation.\n" + creator.SomeOperation());
         }
     }
 }
