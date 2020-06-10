255e601b9805e267813f2dbbf2c61c16a07671e7
Alexander Konovalov <shuryak@protonmail.com>
2020-06-10 12:01:10 +0300

    Added Singleton pattern

---
+++ creational/Singleton/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Singleton
+{
+    internal static class Program
+    {
+        /*
+         * Singleton pattern ensures that there is only one instance of the 
+         * class.
+         *
+         * Also ensures a single access point for all code.
+         */
+        private static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Singleton works!");
+            }
+            else
+            {
+                Console.WriteLine("Singleton bad!");
+            }
+        }
+    }
+}
